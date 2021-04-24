5ac4bcb27b41851aaa1fa38b8fb633fbc1e0b63c
Ayush Dubey <ayushdubey70@gmail.com>
2021-04-24 20:53:33 +0530

    todoey: implement Styling to the Task Screen
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -5,9 +5,32 @@ class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          )
-        );
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('Add Task',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.lightBlueAccent,
+          ),),
+          TextField(
+            autofocus: true,
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+            child: Text('Add'),
+            onPressed: () {
+            //add task
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
