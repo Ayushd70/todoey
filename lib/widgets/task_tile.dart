397c31d8d8eb9798e4cd13939ad9689fb95242c3
Ayush Dubey <ayushdubey70@gmail.com>
2021-04-25 20:14:13 +0530

    todoey: add functionality to check and un-check the task
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -6,9 +6,31 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
     );
   }
 }
