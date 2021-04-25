d1339028b915df9ae6c02d9b7c14d55ef2d19b34
Ayush Dubey <ayushdubey70@gmail.com>
2021-04-25 20:38:51 +0530

    todoey: add a strikethrough to the task
    *on checking upon the task checkbox set strike through the task text
    *move isChecked to Global State
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,36 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool  checkboxState ) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: TaskCheckbox(),
+      title: Text('This is a task.', style: TextStyle(
+        decoration: isChecked ? TextDecoration.lineThrough  : null,
+      ),),
+      trailing: TaskCheckbox(checkboxState : isChecked, toggleCheckboxState : checkboxCallback),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
 
-  @override
-  _TaskCheckboxState createState() => _TaskCheckboxState();
-}
-
-class _TaskCheckboxState extends State<TaskCheckbox> {
-
-  bool isChecked = false;
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue;
-        });
-      },
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
