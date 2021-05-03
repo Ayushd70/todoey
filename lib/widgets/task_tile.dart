333ec3e469d3c3b21c65084d9c14800690a8bc7b
Ayush Dubey <ayushdubey70@gmail.com>
2021-05-03 19:52:27 +0530

    todoey: show a alert dialog on longPress before deleting the task
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -6,12 +6,37 @@ class TaskTile extends StatelessWidget {
   final Function checkboxCallback;
   final Function longPressCallback;
 
-  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback,  this.longPressCallback});
+  TaskTile(
+      {this.isChecked,
+      this.taskTitle,
+      this.checkboxCallback,
+      this.longPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onLongPress: longPressCallback,
+      onLongPress: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Delete Task"),
+                content: Text("Do you want to delete this task?"),
+                actions: [
+                  TextButton(
+                    child: Text("No"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text("Yes"),
+                    onPressed: longPressCallback,
+                  )
+                ],
+              );
+            });
+      },
       title: Text(
         taskTitle,
         style: TextStyle(
